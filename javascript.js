5564f366c961a8b5aa77c18a52ba870ed30755fd
Edvinazz <EdvinasBudrikis@gmail.com>
2020-02-25 19:18:52 +0200

    update
    Pridėtas pop up login ir sign up bei caruselė

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function background1() {
+		    document.body.style.backgroundImage  = "url('img/img1.png')";
+		}
+function background2() {
+		    document.body.style.backgroundImage  = "url('img/img2.jpg')";
+		}
+function background3() {
+		    document.body.style.backgroundImage  = "url('img/img3.jpg')";
+		}
+function background4() {
+		    document.body.style.backgroundImage  = "url('img/img4.jpg')";
+		}
+
+function openFormIN() {
+  document.getElementById("Sign In").style.display = "block";
+}
+
+function openFormUP() {
+  document.getElementById("Sign Up").style.display = "block";
+}
+
+function closeFormIN() {
+  document.getElementById("Sign In").style.display = "none";
+}
+function closeFormUP() {
+  document.getElementById("Sign Up").style.display = "none";
+}
